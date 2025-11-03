1bb3237c1f7cdf79d0b50a522aed5c1ce5177d93
samuelmanurung242 <samuelmanurung242@gmail.com>
2025-11-03 14:42:01 +0700

    elemen baru

--- client/src/pages/mainpage/home.jsx
+++ client/src/pages/mainpage/home.jsx
@@ -2,6 +2,7 @@ export const Mainpage = () => {
     return (
         <div>
             <p className="text-blue-300 text-2xl">tes dari mainpage</p>
+            <h2>testing coyyyy</h2>
         </div>
     )
 }
