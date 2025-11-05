ad375eb470191716415d9a423d3d377c5e5d9dfd
Rashif Dwi <rashif368@gmail.com>
2025-11-05 14:08:55 +0700

    hapus file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,11 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Mainpage from "./pages/mainpage/home";
-import Login from "./pages/mainpage/formlogin";
-import bg from "./assets/pnm_background.jpeg";
 
 function App() {
   return (
-    <div
-      className="min-h-screen bg-cover bg-center"
-      style={{ backgroundImage: `url(${bg})` }}
-    >
+    <div>
       <Routes>
-    
-        <Route path="/formlogin" element={<Login />} />
         <Route path="/" element={<Mainpage />} />
 
 
